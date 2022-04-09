e3da3f0dd4a2278eb2d61abe6aafc964bd0c70d1
Amos <musaamosjoe@gmail.com>
2022-04-08 17:05:53 -0700

    Add 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
