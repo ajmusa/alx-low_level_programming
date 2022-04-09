8c2b810668a3863c374f37381186b9c65ef778c8
Amos <musaamosjoe@gmail.com>
2022-04-08 19:28:37 -0700

    Add 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - putchar and prints all possible different combinations of 2 digits seperated by a comma
+ * Return: 0
+ */
+
+int main(void)
+{
+int c = 0;
+int f_d;
+int l_d;
+while (c <= 99)
+{
+f_d = (c / 10 + '0');
+l_d = (c % 10 + '0');
+
+if (f_d < l_d)
+{
+	putchar(f_d);
+	putchar(l_d);
+
+if (c != 89)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
