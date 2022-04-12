8f505c515a3cd08d2ba9c01784f5eb7c83ff9c16
Amos <musaamosjoe@gmail.com>
2022-04-11 20:03:13 -0700

    Add 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - write and prints a string to the standard error
+ * Return: Return value of 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+		58);
+	return (1);
+}
