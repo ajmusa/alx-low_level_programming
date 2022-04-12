c46d3eadf2517b7dccf41800a934110a29e1e614
Amos <musaamosjoe@gmail.com>
2022-04-11 19:40:14 -0700

    Add 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Uses printf to print out text followed by a new line
+ * Return: Always value of 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
