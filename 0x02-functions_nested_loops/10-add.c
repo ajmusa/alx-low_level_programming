90bd74d6fee00c848830b484e6b09980de7ba49d
Amos <musaamosjoe@gmail.com>
2022-04-13 17:56:22 -0700

    Add 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers.and returns the result
+ *
+ * main - prints the result of the addition of two integers
+ *
+ *@n: keeps track of one integer.
+ *@o: keeps track of the second integer.
+ *
+ * Return: a single output of addition of the two digits
+ */
+
+int add(int n, int o)
+{
+	return (n + o);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void skeleton(int n);
+void print_to_98(int n);
