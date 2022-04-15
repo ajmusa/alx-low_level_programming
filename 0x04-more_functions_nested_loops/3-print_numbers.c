819e2592acef3cfe1f611244f4ee70720321b467
Amos <musaamosjoe@gmail.com>
2022-04-14 21:38:22 -0700

    Update ~: Modify 3-print_numbers.c file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,12 @@
 /**
  * print_numbers - function that prints the numbers, 0 to 9
  * main - prints the numbers 0 to 9
- *@n: parameter holds number
  * Return: value is void
  */
 
 void print_numbers(void)
 {
-	int n = 0;
+	int n;
 	while (n < 10)
 	{
 		_putchar(n + '0');
