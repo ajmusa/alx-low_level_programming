4eb558c257fecd010d34616470d00b9a168e2a7c
Amos <musaamosjoe@gmail.com>
2022-04-14 20:54:11 -0700

    Add 100-prime_factor.c file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ *main - a program that finds and prints the largest prime factor of the number
+ *612852475143, followed by a new line.
+ *Return: value of 0
+ */
+int main(void)
+{
+	long num, a;
+
+	a = 2;
+	num = 612852475143;
+	while (a < sqrt(num))
+	{
+		while (num % a == 0)
+		{
+			num = num / a;
+			printf("%ld %ld\n", num, a);
+		}
+		a++;
+	}
+	printf("%ld\n", num);
+	return (0);
+}
