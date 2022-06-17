8b29520ad1a3d2f564a90fb07400562ae4dcf86d
Amos <musaamosjoe@gmail.com>
2022-06-16 17:53:28 -0700

    Add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the number of arguments passed to function name.
+ * @argc: number of arg in cl
+ * @argv: array of arg in cl
+ * Return: 0
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
