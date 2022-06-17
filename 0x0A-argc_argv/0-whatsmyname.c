308f951dd5145cc8050806cfcf769d6c6c85a425
Amos <musaamosjoe@gmail.com>
2022-06-16 17:47:59 -0700

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print a function's name.
+ * @argc: number of arg in cl
+ * @argv: array of arg in cl
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
