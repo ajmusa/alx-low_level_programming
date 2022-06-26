61e345e4d707fc897029ca632d1a20dd1f3f7518
Amos <musaamosjoe@gmail.com>
2022-06-25 19:14:59 -0700

    Add 100 - 103 .c files & Modify lists.h, 0-p_l.c files

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list.
+ * print_listint - a function or prog that prints all the elements of a list.
  * @h: head of a list.
  *
  * Return: numbers of nodes.
